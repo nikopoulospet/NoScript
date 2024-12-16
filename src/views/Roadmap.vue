2a8297adc8a65f60d0bf53ca43409bef0be01b6b
nikopoulospet <peter@nikopoulos.net>
2024-12-15 20:38:47 -0500

    next phase

--- src/views/Roadmap.vue
+++ src/views/Roadmap.vue
@@ -2,8 +2,10 @@
 <h1>Roadmap</h1>
 <ol>
     <li><s>Stop fucking with the theme</s> <br> GF says its boring but OK</li>
-    <li>Host on nikopoulos.xyz</li>
-    <li>Create a simple development / deployment pipeline </li>
+    <li><s>Host on nikopoulos.xyz</s> <br> This ended up on blog.nikopoulos.xyz because im not yet ready to destroy the microblog sevice although i dont use it anyways</li>
+    <li>Create a simple development / deployment pipeline <br>
+        Im going to need to determine what service or if i plan to self host my docker builds / repo.
+    </li>
     <li>Create a article posting pipeline</li>
     <li>Write 10 articles</li>
 </ol>
